2db14644b8cedf8b8cc985d259db7953b832c59a
yyzidea <yyzidea@hotmail.com>
2021-11-18 16:36:59 +0800

    Add new class 'Chronoamperometry' to generate square wave signal.

--- device/wave_generator.py
+++ device/wave_generator.py
@@ -67,13 +67,13 @@ class WaveGenerator:
             if key == 'waveform':
                 self.send_command('W%sW' % channel, data=value)
             elif key == 'frequency':
-                self.send_command('W%sF' % channel, data=int(value*1e6))
+                self.send_command('W%sF' % channel, data=int(value * 1e6))
             elif key == 'amplitude':
                 self.send_command('W%sA' % channel, data=value)
             elif key == 'offset':
                 self.send_command('W%sO' % channel, data=value)
             elif key == 'duty':
-                self.send_command('W%sD' % channel, data=value)
+                self.send_command('W%sD' % channel, data='%.1f' % value)
             elif key == 'phase':
                 self.send_command('W%sP' % channel, data=value % 360)
             elif key == 'status':
@@ -103,9 +103,9 @@ class CyclicVoltammetry:
         self.V_l = V_l
         self.V_h = V_h
         self.scan_rate = scan_rate
-        self.amplitude = abs(V_h-V_l)
-        self.offset = (V_h+V_l)/2
-        self.frequency = self.scan_rate/self.amplitude/2
+        self.amplitude = abs(V_h - V_l)
+        self.offset = (V_h + V_l) / 2
+        self.frequency = self.scan_rate / self.amplitude / 2
         self.mode = mode
         self.start_from_zero = start_from_zero
 
@@ -124,7 +124,7 @@ class CyclicVoltammetry:
                 warn('Illegal mode, the default mode \'sweep\' will be used.')
 
             if self.start_from_zero and V_l <= 0 <= V_h:
-                self.phase = V_l/self.amplitude*180
+                self.phase = V_l / self.amplitude * 180
             else:
                 self.phase = 0
 
@@ -169,11 +169,80 @@ class CyclicVoltammetry:
         self.wave_generator.__del__()
 
 
+class Chronoamperometry:
+    def __init__(self, port, V_base, V_drive, t_base, t_drive, channel=1, timeout=0.5):
+        self.port = port
+        self.V_base = V_base
+        self.V_drive = V_drive
+        self.t_base = t_base
+        self.t_drive = t_drive
+        self.channel = channel
+        self.timeout = timeout
+
+        self.port = port
+        self.timeout = timeout
+        if hasattr(self, 'wave_generator') is False:
+            self.wave_generator = WaveGenerator(self.port, timeout=self.timeout)
+        self.stop()
+
+        self.amplitude = abs(V_base - V_drive)
+        self.offset = (V_base + V_drive) / 2
+        self.frequency = 1/(t_base+t_drive)
+        if self.V_base < self.V_drive:
+            self.duty = t_drive/(t_base+t_drive)*100
+        else:
+            self.duty = t_base/(t_base+t_drive)*100
+
+        if self.V_base < self.V_drive:
+            self.phase = -t_drive/(t_base+t_drive) * 360
+        else:
+            self.phase = 0
+
+        self.wave_generator.set(self.channel, waveform=1, amplitude=self.amplitude,
+                                offset=self.offset, phase=self.phase, frequency=self.frequency, duty=self.duty)
+
+    def set(self, **kwargs):
+        V_base = self.V_base
+        V_drive = self.V_drive
+        t_base = self.t_base
+        t_drive = self.t_drive
+        channel = self.channel
+        timeout = self.timeout
+
+        for key, value in kwargs.items():
+            if key == 'V_base':
+                V_base = value
+            elif key == 'V_drive':
+                V_drive = value
+            elif key == 't_base':
+                t_base = value
+            elif key == 't_drive':
+                t_drive = value
+            elif key == 'channel':
+                channel = value
+            elif key == 'timeout':
+                timeout = value
+            else:
+                pass
+
+        self.__init__(self.port, V_base, V_drive, t_base, t_drive, channel, timeout)
+
+    def start(self):
+        self.wave_generator.start(self.channel)
+
+    def stop(self):
+        self.wave_generator.stop(self.channel)
+
+    def __del__(self):
+        self.wave_generator.__del__()
+
+
 def waveform_generate(waveform_type, t, amplitude, frequency, offset, phase, **kwargs):
-    t = t-phase/360/frequency
+    t = t - phase / 360 / frequency
     if waveform_type == 'triangle':
-        return 2*amplitude*frequency*np.abs((t+1/2/frequency) % (1/frequency)-1/2/frequency)-amplitude/2+offset
+        return 2 * amplitude * frequency * np.abs(
+            (t + 1 / 2 / frequency) % (1 / frequency) - 1 / 2 / frequency) - amplitude / 2 + offset
     elif waveform_type == 'square':
-        return np.int_(np.sin(2*np.pi*frequency*t) > 0)*amplitude-amplitude/2+offset
+        return np.int_(np.sin(2 * np.pi * frequency * t) > 0) * amplitude - amplitude / 2 + offset
     else:
         return np.zeros_like(t)
