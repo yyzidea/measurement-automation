e71d0471d4395fe6dea1a277fb30d2c19c8c1946
yyzidea <yyzidea@hotmail.com>
2022-11-16 10:50:10 +0800

    Add functions to read and plot spectrum data files.

--- utilities/data_files_viewer.py
+++ utilities/data_files_viewer.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from utilities.data_files_handling import *
-
+from warnings import warn
 
 def spe_bundle_viewer(bundle_filename, count_range=None):
     data_dir, bundle_name = os.path.split(bundle_filename)
@@ -88,4 +88,78 @@ def plot_spe(data, count_range):
     plt.ylabel('Wavelength (nm)')
 
 
+def read_spe_csv(filename, mode='all', noise_level=0):
+    raw_data = np.loadtxt(filename, delimiter=',')
 
+    if raw_data.shape[1] == 2:
+        date_length = raw_data.shape[0]
+        frame_size = 1340
+        while date_length % frame_size:
+            frame_size = frame_size - 1
+
+        data = np.hstack((np.atleast_2d(raw_data[0:frame_size, 0]).transpose(),
+                          np.reshape(raw_data[:, 1], (int(date_length/frame_size), frame_size)).transpose()))
+        frameLen = date_length / frame_size
+    # elif raw_data.shape[1] == 3:
+    #     frames = unique(raw_data(:, 3))'
+    #     data = raw_data(raw_data(:, 3) == frames(1), 1: 2)
+    #     for i = frames(2:end)
+    #     data(:, end + 1) = raw_data(raw_data(:, 3) == i, 2)
+    #     frameLen = length(frames)
+    else:
+        data = raw_data
+        frameLen = raw_data.shape[1] - 1
+
+    if mode == 'acc':
+        data = np.vstack((data[:, 0], np.sum(data[:, 1:-1], 2)))
+    elif mode == 'mean':
+        data = np.vstack((data[:, 0], np.sum(data[:, 1: -1], 2)))
+        data[:, 2] = data[:, 2] / frameLen
+    elif mode == 'raw':
+        data = raw_data
+    elif mode != 'all':
+        warn('Illegal mode: %s, and output with default setting.', mode)
+
+    if noise_level == 'auto':
+        noise_level = np.mean(data[-101:-1, 1])
+
+    data[:, 1:-1] = data[:, 1:-1]-noise_level
+
+    return data
+
+
+def find_spec_peak(data: np.ndarray, spike_limit=500, spike_width=4):
+    peak = 0
+    peak_idx = 0
+    if data.ndim == 2:
+        frame_dim = np.argmin(data.shape)
+        frame_len = data.shape[frame_dim]
+        peak_frame_idx = 0
+        for frame in np.arange(0, frame_len):
+            if frame_dim:
+                temp_peak, temp_peak_idx = find_spec_peak(data[:, frame], spike_limit, spike_width)
+            else:
+                temp_peak, temp_peak_idx = find_spec_peak(data[frame, :], spike_limit, spike_width)
+            if peak < temp_peak:
+                peak = temp_peak
+                peak_idx = temp_peak_idx
+                peak_frame_idx = frame
+        peak_idx = (peak_frame_idx, peak_idx)
+    else:
+        data = data.copy()
+        while 1:
+            peak = np.max(data)
+            peak_idx = np.argmax(data)
+            if peak > spike_limit:
+                data[int(np.floor(peak_idx-spike_width/2)):int(np.ceil(peak_idx+spike_width/2))] = 0
+            else:
+                break
+
+    return peak, peak_idx
+
+
+def plot_spe_from_file(filename, count_range=None):
+    spe = read_spe_csv(filename)
+    if count_range is None:
+        count_range = [0, find_spec_peak(spe[:, 1:-1])[0]]
+    plot_spe(spe, count_range)
